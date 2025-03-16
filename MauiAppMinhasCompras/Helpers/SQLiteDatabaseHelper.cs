53e1eb5a75ceb0ff8430768dd6bf70c13d967d3d
robsonpleitee <robsonpleitee@gmail.com>
2025-03-16 17:32:59 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,49 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        // Insere um novo produto no banco de dados
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        // Atualiza um produto existente no banco de dados
+        public Task<int> Update(Produto p)
+        {
+            return _conn.UpdateAsync(p);
+        }
+
+        // Remove um produto do banco de dados com base no ID
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        // Retorna todos os produtos do banco de dados
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        // Realiza uma busca de produtos com base na descrição
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE ?";
+            return _conn.QueryAsync<Produto>(sql, $"%{q}%");
+        }
+    }
+}
